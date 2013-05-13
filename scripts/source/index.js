08cfa6eed814ffe4bb422f4ba062d591b81947f4
jared laser <laserjn@gmail.com>
2013-05-13 01:54:31 -0400

    marker

--- scripts/source/index.js
+++ scripts/source/index.js
@@ -1,14 +1,47 @@
-/*!
- * index.js - placeholder for source directory
- */
+(function() {
 
-function initialize() {
-    var mapOptions = {
-        center: new google.maps.LatLng(-34.397, 150.644),
-        zoom: 8,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
-    mapOptions);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
+    var latitude = 0;
+    var longitude = 0;
+
+    function initialize() {
+        navigator.geolocation.getCurrentPosition(storeVals);
+    }
+
+    function storeVals(values) {
+        latitude  = values.coords.latitude;
+        longitude = values.coords.longitude;
+        createMapOptions();
+        buildMap();
+    }
+
+    function createMapOptions() {
+        var mapOptions = {
+            center: new google.maps.LatLng(latitude, longitude),
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            keyboardShortcuts: true
+        };
+        return mapOptions;
+    }
+
+    function buildMap() {
+        map = new google.maps.Map(
+            document.getElementById("map-canvas"), createMapOptions()
+        );
+        placeMarker(map);
+    }
+
+    function placeMarker(mapForUse) {
+        var markerOptions = {
+            animation: 'DROP',
+            position: new google.maps.LatLng(latitude, longitude),
+            map: mapForUse
+        }
+
+        var marker = new google.maps.Marker(markerOptions);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+
+ })();
