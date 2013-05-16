9f99c81ce5073cfb1f4235ebcdab7811c63b7544
jared <laserjn@gmail.com>
2013-05-16 17:50:25 -0400

    gotta get some distance calc going

--- scripts/source/index.js
+++ scripts/source/index.js
@@ -1,16 +1,18 @@
-var createdMap   = {};
-var currentRoute = [];
-var Directions   = new google.maps.DirectionsService();
+var createdMap        = {};
+var currentRoute      = [];
+var Directions        = new google.maps.DirectionsService();
+var distanceIndicator = document.getElementById('calc-distance');
+var totalDistance     = 0;
 
 function initialize() {
   navigator.geolocation.getCurrentPosition( abstractLatLong );
 }
 
-function abstractLatLong(data) {
+function abstractLatLong( data ) {
   var latitude  = data.coords.latitude;
   var longitude = data.coords.longitude;
   var latLngObj = new google.maps.LatLng( latitude, longitude );
-  newMap(latLngObj);
+  newMap( latLngObj );
 }
 
 function provideMapOptions( latLngObj ) {
@@ -18,30 +20,28 @@ function provideMapOptions( latLngObj ) {
       center: latLngObj,
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
-      disableDoubleClickZoom: true
+      disableDoubleClickZoom: true,
+      draggableCursor: 'crosshair'
   };
   return mapOptions;
 }
 
-function newMap(latLngObj) {
-  createdMap = new google.maps.Map( document.getElementById( "map-canvas" ),
+function newMap( latLngObj ) {
+  createdMap = new google.maps.Map( document.getElementById( 'map-canvas' ),
     provideMapOptions(latLngObj) );
   createMapEvents( createdMap );
 }
 
 function createMapEvents( map ) {
-  google.maps.event.addDomListener( map, 'click', function( latLngObj ) {
-    createMarkerOptions( latLngObj );
+  google.maps.event.addDomListener( map, 'click', function( response ) {
+    createMarkerOptions( response.latLng );
   });
 }
 
-function createMarkerOptions( latLngObj ) {
-  var latitude = latLngObj.latLng.kb;
-  var longitude = latLngObj.latLng.lb;
-  var gmapsLatLong = new google.maps.LatLng( latitude, longitude );
+function createMarkerOptions( latLng ) {
 
   var markerOptions = {
-    position: gmapsLatLong,
+    position: latLng,
     map: createdMap
   }
 
@@ -49,10 +49,10 @@ function createMarkerOptions( latLngObj ) {
 
   // only construct route if this isn't the first point
   if ( currentRoute.length ) {
-    constructRouteRequest( gmapsLatLong );
+    constructRouteRequest( latLng );
   }
 
-  currentRoute.push( gmapsLatLong );
+  currentRoute.push( latLng );
 }
 
 function createMarker( options ) {
@@ -61,7 +61,7 @@ function createMarker( options ) {
 
 function constructRouteRequest( destination ) {
   var previousPoint = currentRoute[currentRoute.length - 1];
-  var newPoint      = destination;
+  var newPoint = destination;
   var directionsRequest = {
     avoidHighways: true,
     travelMode: google.maps.TravelMode.WALKING,
@@ -69,20 +69,32 @@ function constructRouteRequest( destination ) {
     destination: newPoint
   }
   Directions.route( directionsRequest, function( result, status ) {
-    drawRoute();
+    var route = result.routes[0];
+    drawRoute( route );
   });
 }
 
-function drawRoute() {
+function drawRoute( route ) {
   var lineOptions = {
       clickable: false,
       map: createdMap,
       strokeOpacity: 1,
-      strokeWeight: 3,
-      strokeColor: '00264c',
-      path: currentRoute
+      strokeWeight: 4,
+      strokeColor: 'cc2644',
+      path: route.overview_path
   };
   new google.maps.Polyline( lineOptions );
+  addDistance( route.legs[0].distance );
+}
+
+function addDistance( distance ) {
+  var metricValue = distance.value;
+  distanceIndicator.textContent = convertMetersToMiles( metricValue ) + 'mi';
+}
+
+function convertMetersToMiles( meters ) {
+  var miles = 0.000621371 * meters;
+  return miles;
 }
 
 google.maps.event.addDomListener( window, 'load', initialize );
